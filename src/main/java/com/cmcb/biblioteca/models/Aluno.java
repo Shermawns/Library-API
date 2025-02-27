409b668c097383ad174fe19baf2430014702a822
sherm <shermawns@gmail.com>
2025-02-27 15:25:18 -0300

    feat: Criação de entidade Aluno

---
+++ src/main/java/com/cmcb/biblioteca/models/Aluno.java
@@ -0,0 +1,74 @@
+package com.cmcb.biblioteca.models;
+
+import jakarta.persistence.*;
+import org.springframework.cglib.core.Local;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_aluno")
+public class Aluno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String matricula;
+    private String nome;
+    @CreatedDate
+    private LocalDate data_criacao;
+    @LastModifiedDate
+    private LocalDate data_atualizacao;
+
+    public Aluno() {
+    }
+
+    public Aluno(Long id, String matricula, String nome, LocalDate data_criacao, LocalDate data_atualizacao) {
+        this.id = id;
+        this.matricula = matricula;
+        this.nome = nome;
+        this.data_criacao = data_criacao;
+        this.data_atualizacao = data_atualizacao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getData_criacao() {
+        return data_criacao;
+    }
+
+    public void setData_criacao(LocalDate data_criacao) {
+        this.data_criacao = data_criacao;
+    }
+
+    public LocalDate getData_atualizacao() {
+        return data_atualizacao;
+    }
+
+    public void setData_atualizacao(LocalDate data_atualizacao) {
+        this.data_atualizacao = data_atualizacao;
+    }
+}
