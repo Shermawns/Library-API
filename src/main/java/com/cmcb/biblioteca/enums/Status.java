3880a4c147457050b688af1251e0a47c91672ce1
sherm <shermawns@gmail.com>
2025-03-05 10:49:01 -0300

    feat: Criando classe Livro

---
+++ src/main/java/com/cmcb/biblioteca/enums/Status.java
@@ -0,0 +1,6 @@
+package com.cmcb.biblioteca.enums;
+
+public enum Status {
+    DISPONIVEL,
+    ALUGADO
+}
---
+++ src/main/java/com/cmcb/biblioteca/models/Livro.java
@@ -0,0 +1,58 @@
+package com.cmcb.biblioteca.models;
+
+import com.cmcb.biblioteca.enums.Status;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_livro")
+public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titulo;
+    private Long codigo;
+    private Status status;
+
+    public Livro() {
+    }
+
+    public Livro(Long id, String titulo, Long codigo, Status status) {
+        this.id = id;
+        this.titulo = titulo;
+        this.codigo = codigo;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+}
